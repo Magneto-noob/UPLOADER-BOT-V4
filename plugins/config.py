e1fc2b6ed622f6c692574fd62e784989e072d910
Magneto-noob <87361323+Magneto-noob@users.noreply.github.com>
2025-03-04 17:07:12 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -37,7 +37,7 @@ class Config(object):
     LOGGER = logging
     OWNER_ID = "910674886"
     SESSION_NAME = "UploaderXNTBot"
-    UPDATES_CHANNEL = "savecintent"
+    UPDATES_CHANNEL = "-1001540083291"
 
     TG_MIN_FILE_SIZE = 2194304000
     BOT_USERNAME = "Linkxtbot"
